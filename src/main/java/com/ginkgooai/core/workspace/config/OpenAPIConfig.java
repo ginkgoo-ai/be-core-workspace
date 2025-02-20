de87c9412695ced944d0b102690265f4b051347b
bruce <bruce.zhang@biteinvestments.com>
2025-02-20 14:15:55 +0800

    feature:Springdoc support securitySchemes

--- src/main/java/com/ginkgooai/core/workspace/config/OpenAPIConfig.java
+++ src/main/java/com/ginkgooai/core/workspace/config/OpenAPIConfig.java
@@ -1,7 +1,10 @@
 package com.ginkgooai.core.workspace.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +18,19 @@ public class OpenAPIConfig {
     @Value("${core-gateway-public-uri}")
     private String gatewayUri;
 
+    private static final String COOKIE_AUTH_NAME = "cookieAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .servers(List.of(new Server().url(gatewayUri + "/api/workspace")))
-                .info(new Info().title("Workspace Service API").version("1.0.0"));
+                .info(new Info().title("Workspace Service API").version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(COOKIE_AUTH_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(COOKIE_AUTH_NAME, new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.COOKIE)
+                                .name("SESSION")
+                                .description("Session cookie for authentication")));
     }
-
 }
